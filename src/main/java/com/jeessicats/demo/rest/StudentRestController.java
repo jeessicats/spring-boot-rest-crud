0c7d024f6b205b9e107d6a1a949293f6fa1727d0
jeessicats <jeessicats@gmail.com>
2025-02-13 16:38:20 -0300

    Exception Handling in Spring

--- src/main/java/com/jeessicats/demo/rest/StudentRestController.java
+++ src/main/java/com/jeessicats/demo/rest/StudentRestController.java
@@ -2,10 +2,9 @@ package com.jeessicats.demo.rest;
 
 import com.jeessicats.demo.entity.Student;
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,8 +38,13 @@ public class StudentRestController {
     public Student getStudent(@PathVariable int studentId) {
 
         // just index into the list ... keep it simple for now
+
+        // check the studentId against list size
+        if ((studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
         return theStudents.get(studentId);
     }
-
 
 }
