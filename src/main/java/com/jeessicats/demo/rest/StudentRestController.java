f4331d6133d4eb36730a65b3198881c1c4523e8c
jeessicats <jeessicats@gmail.com>
2025-02-13 14:46:48 -0300

    first commit

---
+++ src/main/java/com/jeessicats/demo/rest/StudentRestController.java
@@ -0,0 +1,46 @@
+package com.jeessicats.demo.rest;
+
+import com.jeessicats.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    // define @PostConstruct to load the student data only once
+    @PostConstruct
+    public void loadData() {
+
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("Jane", "Willow"));
+        theStudents.add(new Student("Mary", "Green"));
+        theStudents.add(new Student("James", "Bond"));
+    }
+
+    // define and endpoint for "/students" - return list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+
+        return theStudents;
+    }
+
+    // define an endpoint for "/students/{studentID}" - return a single student by index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list ... keep it simple for now
+        return theStudents.get(studentId);
+    }
+
+
+}
